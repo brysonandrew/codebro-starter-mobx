28964791bb10f9138c3e8699cc1c46a434a22071
Andrew <andrewbryson12@gmail.com>
2018-01-23 22:01:43 +0100

    init

--- src/app/widgets/MotionScroll/ScrollSink.tsx
+++ src/app/widgets/MotionScroll/ScrollSink.tsx
@@ -5,12 +5,21 @@ interface IProps {
 }
 
 export class ScrollSink extends React.Component<IProps, any> {
+
+    scrollMethod = document[!!document.scrollingElement ? "scrollingElement" : !!document.documentElement ? "documentElement" : "body"].scrollTop;
+
     public constructor(props?: any, context?: any) {
         super(props, context);
     }
+
     componentDidUpdate(prevProps) {
         if (prevProps.scrollTop !== this.props.scrollTop) {
-            document.scrollingElement.scrollTop = this.props.scrollTop;
+
+            if (!!this.scrollMethod) {
+                document[!!document.scrollingElement ? "scrollingElement" : !!document.documentElement ? "documentElement" : "body"].scrollTop = this.props.scrollTop;
+            } else {
+                window[!!window.scrollTo ? `scrollTo` : `scroll`](0, this.props.scrollTop);
+            }
         }
     }
     render(): JSX.Element {
